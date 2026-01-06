4ee92be2a82255455b9ac44d54dcaec1695a7c95
Chase Manning <chase@manning.dev>
2026-01-06 17:02:48 +0000

    :rocket: add package

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import addresses from "../addresses.json";
+import leveragedTokenAbi from "../abis/leveraged-token-abi.json";
+import factoryAbi from "../abis/factory-abi.json";
+
+// Address type for Ethereum addresses
+export type Address = `0x${string}`;
+
+// Export all contract addresses
+export const ALL_ADDRESSES = addresses as {
+  Factory: Address;
+  GlobalStorage: Address;
+  GlobalStorageHelper: Address;
+  HyperliquidHandler: Address;
+  LeveragedTokenHelper: Address;
+  LeveragedTokenImplementation: Address;
+  Referrals: Address;
+};
+
+// Export individual addresses for convenience
+export const FACTORY_ADDRESS = ALL_ADDRESSES.Factory;
+export const GLOBAL_STORAGE_ADDRESS = ALL_ADDRESSES.GlobalStorage;
+export const GLOBAL_STORAGE_HELPER_ADDRESS = ALL_ADDRESSES.GlobalStorageHelper;
+export const HYPERLIQUID_HANDLER_ADDRESS = ALL_ADDRESSES.HyperliquidHandler;
+export const LEVERAGED_TOKEN_HELPER_ADDRESS =
+  ALL_ADDRESSES.LeveragedTokenHelper;
+export const LEVERAGED_TOKEN_IMPLEMENTATION_ADDRESS =
+  ALL_ADDRESSES.LeveragedTokenImplementation;
+export const REFERRALS_ADDRESS = ALL_ADDRESSES.Referrals;
+
+// Export ABIs
+export const LEVERAGED_TOKEN_ABI = leveragedTokenAbi;
+export const FACTORY_ABI = factoryAbi;
+
+// Export addresses as default for convenience
+export default ALL_ADDRESSES;
