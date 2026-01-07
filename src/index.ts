fbbdc12490e28204299e524d79035d90acd3d5b3
Chase Manning <chase@manning.dev>
2026-01-07 16:19:17 +0000

    :rocket: add usdc address and erc20 abi

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import globalStorageHelperAbi from "../abis/global-storage-helper-abi.json";
 import hyperliquidHandlerAbi from "../abis/hyperliquid-handler-abi.json";
 import leveragedTokenHelperAbi from "../abis/leveraged-token-helper-abi.json";
 import referralsAbi from "../abis/referrals-abi.json";
+import erc20Abi from "../abis/erc20-abi.json";
 
 // Address type for Ethereum addresses
 export type Address = `0x${string}`;
@@ -19,6 +20,7 @@ export const ALL_ADDRESSES = addresses as {
   LeveragedTokenHelper: Address;
   LeveragedTokenImplementation: Address;
   Referrals: Address;
+  USDC: Address;
 };
 
 // Export individual addresses for convenience
@@ -31,6 +33,7 @@ export const LEVERAGED_TOKEN_HELPER_ADDRESS =
 export const LEVERAGED_TOKEN_IMPLEMENTATION_ADDRESS =
   ALL_ADDRESSES.LeveragedTokenImplementation;
 export const REFERRALS_ADDRESS = ALL_ADDRESSES.Referrals;
+export const USDC_ADDRESS = ALL_ADDRESSES.USDC;
 
 // Export ABIs
 export const FACTORY_ABI = factoryAbi;
@@ -40,6 +43,7 @@ export const HYPERLIQUID_HANDLER_ABI = hyperliquidHandlerAbi;
 export const LEVERAGED_TOKEN_ABI = leveragedTokenAbi;
 export const LEVERAGED_TOKEN_HELPER_ABI = leveragedTokenHelperAbi;
 export const REFERRALS_ABI = referralsAbi;
+export const USDC_ABI = erc20Abi;
 
 // Export addresses as default for convenience
 export default ALL_ADDRESSES;
